2d4a66d6d9cc494dcd0c63902874a5fde34925a5
XFKR <xfkr@qq.com>
2024-08-29 19:45:46 +0800

    这下应该好了

--- out/build/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Code/CPlusPlus/Student_Random
-# Build directory: D:/Code/CPlusPlus/Student_Random/out/build/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("DataStruct")
--- out/build/build/DataStruct/CMakeFiles/DataStruct.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libDataStruct.a"
-)
