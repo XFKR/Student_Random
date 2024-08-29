ad2d518b6fc5394f826f04896ca0e8b65116e086
XFKR <xfkr@qq.com>
2024-08-29 19:40:43 +0800

    无语了这Git

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<random>
 #include"./DataStruct/students.hpp"
 
 int main(int argc, char* argv[])
@@ -6,6 +7,7 @@ int main(int argc, char* argv[])
     if (argc!=2) return -1;
 
     Students sds(fopen(argv[1], "r"));
+
 
 
 
---
+++ out/build/build/CMakeFiles/Student_Random.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Student_Random.
+# This may be replaced when dependencies are built.
