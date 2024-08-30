adb4eeb15be4b7780802bb34aa9666092e291eaf
XFKR <xfkr@qq.com>
2024-08-30 11:22:23 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -10,14 +10,12 @@ int main(int argc, char* argv[])
     system("chcp 936");
     if (argc!=3) return -1;//参数不足 不运行
 
-    Students stdt(fopen(argv[1], "r"));
+    Students stdt(fopen(argv[1], "r")); //初始化
 
     if (atoi(argv[2])>=stdt.getlen()) return -1;//参数错误 直接退出
 
     std::vector<int> rd;
     for (int i=1; i<=stdt.getlen(); i++) rd.push_back(i); //顺序排序写入数组
-
-    std::random_shuffle(rd.begin(), rd.end()); //打乱数组
 
     std::mt19937 rdg((int)time(NULL)); //创建伪随机数生成器 以时间戳为种子
     std::uniform_int_distribution distrib(1, 100); //限定打乱次数
@@ -27,7 +25,7 @@ int main(int argc, char* argv[])
     }
 
     for (int i=0; i<=atoi(argv[2])-1; i++){
-        printf("%s %d\n", stdt.getname(rd[i]).c_str(), stdt.getid(rd[i]));
+        printf("%s %d\n", stdt.getname(rd[i]).c_str(), stdt.getid(rd[i])); //输出
     }
 
     return 0;
